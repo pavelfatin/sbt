2de920969fa88896f8ce343c3e7d19c70a685df7
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-02-14 13:59:37 +0400

    ClasspathUtilities: code that depends on scala-compiler.jar is moved to PathUtilities

--- util/classpath/src/main/scala/sbt/classpath/ClasspathUtilities.scala
+++ util/classpath/src/main/scala/sbt/classpath/ClasspathUtilities.scala
@@ -69,14 +69,7 @@ object ClasspathUtilities
 	
 	def isArchive(file: File): Boolean = isArchiveName(file.getName)
 	def isArchiveName(fileName: String) = fileName.endsWith(".jar") || fileName.endsWith(".zip")
-	// Partitions the given classpath into (jars, directories)
-	private[sbt] def separate(paths: Iterable[File]): (Iterable[File], Iterable[File]) = paths.partition(isArchive)
-	// Partitions the given classpath into (jars, directories)
-	private[sbt] def buildSearchPaths(classpath: Iterable[File]): (collection.Set[File], collection.Set[File]) =
-	{
-		val (jars, dirs) = separate(classpath)
-		(linkedSet(jars ++ extraJars), linkedSet(dirs ++ extraDirs))
-	}
+
 	private[sbt] def onClasspath(classpathJars: collection.Set[File], classpathDirectories: collection.Set[File], file: File): Boolean =
 	{
 		val f = file.getCanonicalFile
@@ -110,33 +103,5 @@ object ClasspathUtilities
 			}
 		}
 		catch { case e: Exception => Nil }
-	}
-	
-	private lazy val (extraJars, extraDirs) =
-	{
-		import scala.tools.nsc.GenericRunnerCommand
-		val settings = (new GenericRunnerCommand(Nil, message => error(message))).settings
-		val bootPaths = IO.pathSplit(settings.bootclasspath.value).map(p => new File(p)).toList
-		val (bootJars, bootDirs) = separate(bootPaths)
-		val extJars =
-		{
-			val buffer = new ListBuffer[File]
-			def findJars(dir: File)
-			{
-				buffer ++= dir.listFiles(new SimpleFileFilter(isArchive))
-				for(dir <- dir.listFiles(DirectoryFilter))
-					findJars(dir)
-			}
-			for(path <- IO.pathSplit(settings.extdirs.value); val dir = new File(path) if dir.isDirectory)
-				findJars(dir)
-			buffer.readOnly.map(_.getCanonicalFile)
-		}
-		(linkedSet(extJars ++ bootJars), linkedSet(bootDirs))
-	}
-	private def linkedSet[T](s: Iterable[T]): Set[T] =
-	{
-		val set: mutable.Set[T] = JavaConversions.asScalaSet(new java.util.LinkedHashSet[T])
-		set ++= s
-		set
 	}
 }
